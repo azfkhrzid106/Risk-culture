f4b3650242287110fa28d53ac2388b6301346932
Azri fakhrezi damanik <121395806+azfkhrzid106@users.noreply.github.com>
2025-12-11 19:37:59 +0700

    Fix HTTPS mixed content

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,24 +2,16 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
+    public function boot()
     {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        Vite::prefetch(concurrency: 3);
+        // Force HTTPS di production
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
